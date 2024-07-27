f2f30b2de28d15f31a4a64bf9133a89d884b87de
Ryo-0912 <tensaisander0912@gmail.com>
2024-07-27 10:23:55 +0900

    add comment

--- 10-logger/main.go
+++ 10-logger/main.go
@@ -7,17 +7,17 @@ import (
 )
 
 func main() {
-	file, err := os.Create("log.txt")
+	file, err := os.Create("log.txt") // ファイル生成
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer file.Close()
-	flags := log.Lshortfile
+	flags := log.Lshortfile // 発生したエラーの行数追加
 	// flags := log.Lshortfile | log.LstdFlags
-	warnLogger := log.New(io.MultiWriter(file, os.Stderr), "WARN: ", flags)
+	warnLogger := log.New(io.MultiWriter(file, os.Stderr), "WARN: ", flags) // ログ作成
 	errorLogger := log.New(io.MultiWriter(file, os.Stderr), "ERROR: ", flags)
 
 	warnLogger.Println("warning A")
 
-	errorLogger.Fatalln("critical error")
+	errorLogger.Fatalln("critical error") // エラーログ書き込んだ上でプログラム強制終了
 }
